dc1aeda502ba3cc6f71c50b4d3c361ff14d9ce8a
maxdu1998 <maxdu1998@ucl.br>
2022-06-09 17:58:21 -0300

    ✨💩  Função de Cadastro

--- lib/views/models/user.dart
+++ lib/views/models/user.dart
@@ -3,11 +3,19 @@ class User {
   String name = "";
   String email = "";
   String password = "";
+  String imageUrl = "";
+  String phone = "";
 
   User();
 
   Map<String, dynamic> toMap() {
-    Map<String, dynamic> map = {"idUser": idUser, "name": name, "email": email};
+    Map<String, dynamic> map = {
+      "idUser": idUser,
+      "name": name,
+      "email": email,
+      "imageUrl": imageUrl,
+      "phone": phone
+    };
     return map;
   }
 }
--- lib/views/register.dart
+++ lib/views/register.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:melhor_negocio/views/custom_input.dart';
+import 'package:melhor_negocio/views/models/user.dart' as u;
+import 'package:firebase_auth/firebase_auth.dart';
+
+File _imagePicked = File("images/logo.png");
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -8,6 +15,69 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  Future _imagePicker() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    final pickedImageFile = File(pickedImage!.path);
+    setState(() {
+      _imagePicked = pickedImageFile;
+    });
+  }
+
+  final TextEditingController _controllerEmail =
+      TextEditingController(text: "teste@ucl.br");
+
+  final TextEditingController _controllerPassword =
+      TextEditingController(text: "123456@");
+
+  final TextEditingController _controllerConfirmPassword =
+      TextEditingController(text: "123456@");
+
+  final TextEditingController _controllerName =
+      TextEditingController(text: "User Teste");
+
+  final TextEditingController _controllerPhone =
+      TextEditingController(text: "40028922");
+
+  String _errorMessage = "";
+
+  _userRegister(u.User user) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    auth
+        .createUserWithEmailAndPassword(
+            email: user.email, password: user.password)
+        .then((firebaseUser) {
+      auth
+          .signInWithEmailAndPassword(
+              email: user.email, password: user.password)
+          .then((firebaseUser) {
+        Navigator.pushReplacementNamed(context, "");
+      });
+    });
+  }
+
+  _fieldValidation() {
+    String email = _controllerEmail.text;
+    String password = _controllerPassword.text;
+    String name = _controllerName.text;
+    String phone = _controllerPhone.text;
+
+    if (email.isNotEmpty && password.isNotEmpty) {
+      u.User user = u.User();
+      user.email = email;
+      user.password = password;
+      user.name = name;
+      user.phone = phone;
+      _userRegister(user);
+    } else {
+      setState(() {
+        _errorMessage = "Preencha o e-mail e senha!";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,75 +91,58 @@ class _RegisterState extends State<Register> {
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                      child: TextField(
+                  ElevatedButton(
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      child: Text(
+                        "Imagem de Perfil",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                    onPressed: () {
+                      _imagePicker();
+                    },
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                      child: Image.file(_imagePicked, width: 160, height: 160)),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                      child: CustomInput(
+                        controller: _controllerName,
+                        hint: "Nome Completo",
                         autofocus: true,
-                        keyboardType: TextInputType.text,
-                        style: TextStyle(fontSize: 20),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                            hintText: "Nome Completo",
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)))),
+                        type: TextInputType.text,
                       )),
-                  const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                      child: TextField(
-                        autofocus: true,
-                        keyboardType: TextInputType.emailAddress,
-                        style: TextStyle(fontSize: 20),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                            hintText: "E-mail",
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)))),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                      child: CustomInput(
+                        controller: _controllerEmail,
+                        hint: "E-mail",
+                        type: TextInputType.emailAddress,
                       )),
-                  const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                      child: TextField(
-                        obscureText: true,
-                        keyboardType: TextInputType.text,
-                        style: TextStyle(fontSize: 20),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                            hintText: "Senha",
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)))),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                      child: CustomInput(
+                        controller: _controllerPassword,
+                        hint: "Senha",
+                        obscure: true,
+                        type: TextInputType.visiblePassword,
                       )),
-                  const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                      child: TextField(
-                        obscureText: true,
-                        keyboardType: TextInputType.text,
-                        style: TextStyle(fontSize: 20),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                            hintText: "Repita a Senha",
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)))),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                      child: CustomInput(
+                        controller: _controllerConfirmPassword,
+                        hint: "Repita a Senha",
+                        obscure: true,
+                        type: TextInputType.visiblePassword,
                       )),
-                  const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 16),
-                      child: TextField(
-                        autofocus: true,
-                        keyboardType: TextInputType.phone,
-                        style: TextStyle(fontSize: 20),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                            hintText: "Telefone",
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)))),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                      child: CustomInput(
+                        controller: _controllerPhone,
+                        hint: "Número de Celular",
+                        type: TextInputType.phone,
                       )),
                   ElevatedButton(
                     child: const Padding(
@@ -99,7 +152,9 @@ class _RegisterState extends State<Register> {
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      _fieldValidation();
+                    },
                   )
                 ]),
           ))),
