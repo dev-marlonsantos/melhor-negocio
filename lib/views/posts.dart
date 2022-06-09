c1c416dedfe21bd827cdbaf42d22fa2a4fca092c
maxdu1998 <maxdu1998@ucl.br>
2022-06-08 21:13:49 -0300

    ✨ Criação da Tela de Anúncios

---
+++ lib/views/posts.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Posts extends StatefulWidget {
+  const Posts({Key? key}) : super(key: key);
+
+  @override
+  State<Posts> createState() => _PostsState();
+}
+
+class _PostsState extends State<Posts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("Anúncios"),
+      color: Colors.white,
+    );
+  }
+}
